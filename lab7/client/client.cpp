50c477b3532cd519675ef54a476ae911d9c70456
Marya <Marya@DESKTOP-TDKCMV7>
2023-11-15 22:32:36 +0300

    added lab7 and lab8

---
+++ lab7/client/client.cpp
@@ -0,0 +1,63 @@
+﻿#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <thread>
+#include <string>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+const int BUF_SIZE = 4096;
+
+// Функция для обработки входящих сообщений
+void receiveMessages(SOCKET clientSocket) {
+    char buffer[BUF_SIZE];
+    while (true) {
+        ZeroMemory(buffer, BUF_SIZE);
+        int bytesReceived = recv(clientSocket, buffer, BUF_SIZE, 0);
+        if (bytesReceived > 0) {
+            std::cout << "user2> " << std::string(buffer, 0, bytesReceived) << std::endl;
+        }
+        // В реальном приложении здесь должна быть проверка на ошибки и закрытие соединения при необходимости
+    }
+}
+
+int main() {
+    WSADATA wsaData;
+    SOCKET clientSocket;
+    sockaddr_in serverAddr;
+
+    // Инициализация Winsock
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    // Создание клиентского сокета
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Задание адреса и порта сервера
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(54000); // Порт сервера
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); // IP сервера
+
+    // Подключение к серверу
+    connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+
+    // Запуск потока для приема сообщений
+    std::thread receiveThread(receiveMessages, clientSocket);
+
+    // Основной цикл для отправки сообщений
+    std::string userInput;
+    while (true) {
+        std::getline(std::cin, userInput);
+        if (userInput == "exit") {
+            break; // Выход из цикла, если пользователь ввел "exit"
+        }
+        send(clientSocket, userInput.c_str(), userInput.size() + 1, 0);
+        // В реальном приложении здесь должна быть проверка на ошибки и закрытие соединения при необходимости
+    }
+
+    // Закрытие потока и сокета
+    receiveThread.detach();
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ lab7/server/server.cpp
@@ -0,0 +1,73 @@
+﻿#include <WinSock2.h>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+const int BUF_SIZE = 4096;
+std::vector<SOCKET> clientSockets; // Вектор для хранения сокетов клиентов
+std::vector<std::thread> threads;   // Вектор для хранения потоков
+
+// Обработчик клиента
+void handleClient(SOCKET clientSocket) {
+    char buffer[BUF_SIZE];
+    while (true) {
+        ZeroMemory(buffer, BUF_SIZE);
+        int bytesReceived = recv(clientSocket, buffer, BUF_SIZE, 0);
+        if (bytesReceived <= 0) {
+            // Обработка ошибок или закрытие соединения
+            break;
+        }
+        // Пересылка сообщения другим клиентам
+        for (const auto& otherClientSocket : clientSockets) {
+            if (otherClientSocket != clientSocket) {
+                send(otherClientSocket, buffer, bytesReceived, 0);
+            }
+        }
+    }
+    closesocket(clientSocket);
+}
+
+int main() {
+    WSADATA wsaData;
+    SOCKET serverSocket;
+    sockaddr_in serverAddr;
+    sockaddr_in clientAddr;
+    int clientAddrSize = sizeof(clientAddr);
+
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(54000);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+
+    listen(serverSocket, 2);
+
+    // Принимаем два соединения и создаем для них потоки
+    for (int i = 0; i < 2; i++) {
+        SOCKET clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientAddrSize);
+        if (clientSocket == INVALID_SOCKET) {
+            std::cerr << "Error accepting client: " << WSAGetLastError() << std::endl;
+            continue;
+        }
+
+        clientSockets.push_back(clientSocket);
+        threads.push_back(std::thread(handleClient, clientSocket));
+    }
+
+    // Ожидаем завершения работы потоков
+    for (auto& t : threads) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
